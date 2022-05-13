902c5db0b871a3838b8c1e2cc32ddba2e791d05f
sristis0202 <51877528+sristis0202@users.noreply.github.com>
2022-05-13 13:48:19 +0530

    optimised

--- Day4/493. Reverse Pairs.cpp
+++ Day4/493. Reverse Pairs.cpp
@@ -4,6 +4,67 @@ using namespace std;
 
 class Solution {
 public:
+
+    int Merge(vector<int>&nums, int low, int mid, int high){
+        int total = 0;
+        int j = mid+1;
+        for(int i=low; i<=mid; i++){
+            while(j<=high && nums[i] > 2 LL *nums[j]){
+                j++;
+            }
+            total += (j-(mid+1));
+        }
+        vector<int> t;
+        int left = low, right = mid+1;
+        while (left <= mid && right <= high) {
+            if (nums[left] <= nums[right]) {
+                t.push_back(nums[left++]);
+            } 
+            else {
+                t.push_back(nums[right++]);
+            }
+        }
+        while (left <= mid) {
+            t.push_back(nums[left++]);
+        }
+        while (right <= high) {
+            t.push_back(nums[right++]);
+        }
+
+        for (int i = low; i <= high; i++) {
+            nums[i] = t[i - low];
+        }
+        return total;
+
+    }
+
+    int MergeSort(vector < int > & nums, int low, int high) {
+
+        if (low >= high) return 0;
+        int mid = (low + high) / 2;
+        int inv = MergeSort(nums, low, mid); //left
+        inv += MergeSort(nums, mid + 1, high); //right
+        inv += Merge(nums, low, mid, high); //merging
+        
+        return inv;
+    }
+    int reversePairs(vector<int>& nums) {
+        return MergeSort(arr, 0, arr.size() - 1);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+/*class Solution {
+public:
     int reversePairs(vector<int>& nums) {
         int ans=0;
         for(int i=0; i<nums.size()-1; i++){
@@ -13,4 +74,4 @@ public:
         }
         return ans;
     }
-};
+};*/
